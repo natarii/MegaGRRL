17010396596de34a8c67d4ea1955edd27e62f438
Agiri <agiri@agiri.ninja>
2018-07-14 14:07:08 -0400

    add pin defines

---
+++ main/pins.h
@@ -0,0 +1,14 @@
+#ifndef AGR_PINS_H
+#define AGR_PINS_H
+
+#define PIN_DRIVER_SHCLK 19
+#define PIN_DRIVER_DATA  23
+#define PIN_DRIVER_SHSTO 18
+
+#define PIN_OLED_CS     21
+#define PIN_OLED_DC     22
+#define PIN_OLED_CLK    32
+#define PIN_OLED_DATA   27
+#define PIN_OLED_RST    99999
+
+#endif
