71f343a46e9129ad57f258b52be3a9998c4fa241
Agiri <agiri@agiri.ninja>
2018-07-16 04:51:40 -0400

    remove oled references from pin defs

--- main/include/pins.h
+++ main/include/pins.h
@@ -5,12 +5,6 @@
 #define PIN_DRIVER_DATA  23
 #define PIN_DRIVER_SHSTO 18
 
-#define PIN_OLED_CS     21
-#define PIN_OLED_DC     22
-#define PIN_OLED_CLK    32
-#define PIN_OLED_DATA   27
-#define PIN_OLED_RST    99999
-
 #define PIN_I2C_CLK     16
 #define PIN_I2C_DATA    17
 
