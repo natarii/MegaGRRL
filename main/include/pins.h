91203d51d1cfa5764d152872a11ec399b97cb1ab
Agiri <agiri@agiri.ninja>
2018-07-16 05:12:42 -0400

    set up display pins and add defs to be compatible with loboris library

--- main/include/pins.h
+++ main/include/pins.h
@@ -13,4 +13,22 @@
 #define PIN_BAT_SENSE    34
 #define PIN_BAT_SENSE_EN 5
 
+#define PIN_DISP_DC     22
+#define PIN_DISP_CS     21
+#define PIN_DISP_SCK    32
+#define PIN_TOUCH_CS    25
+#define PIN_DISP_MISO   26
+#define PIN_DISP_MOSI   27
+
+//hacks for loboris tft library:
+#define TFT_PIN_NUM_MISO PIN_DISP_MISO
+#define TFT_PIN_NUM_MOSI PIN_DISP_MOSI
+#define TFT_PIN_NUM_CLK  PIN_DISP_SCK
+#define TFT_PIN_NUM_CS   PIN_DISP_CS
+#define TFT_PIN_NUM_DC   PIN_DISP_DC
+#define TFT_PIN_NUM_TCS  PIN_TOUCH_CS
+#define TFT_PIN_NUM_RST  0 //disabled
+#define TFT_PIN_NUM_BCKL 0 //disabled
+
+
 #endif
