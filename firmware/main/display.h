4cfc2618608befd4fc1851e4160ab8344f4f96bf
natalie <webmistress@natalie.ee>
2019-06-11 17:20:07 -0400

    yeet old lcd library stuff

--- firmware/main/display.h
+++
@@ -1,7 +0,0 @@
-#ifndef AGR_DISPLAY_H
-#define AGR_DISPLAY_H
-#include "freertos/FreeRTOS.h"
-
-bool DisplayMgr_Setup();
-
-#endif
---
+++ firmware/main/pins.h
@@ -0,0 +1,21 @@
+#ifndef AGR_PINS_H
+#define AGR_PINS_H
+
+#define PIN_DRIVER_SHCLK 19
+#define PIN_DRIVER_DATA  23
+#define PIN_DRIVER_SHSTO 18
+
+#define PIN_I2C_CLK     16
+#define PIN_I2C_DATA    17
+
+#define PIN_IOEXP_INT   35
+
+#define PIN_BAT_SENSE    34
+#define PIN_BAT_SENSE_EN 5
+
+#define PIN_DISP_DC     22
+#define PIN_DISP_CS     21
+#define PIN_DISP_SCK    32
+#define PIN_DISP_MOSI   27
+
+#endif
