7b0bd9c9fdc0b84ddfe96d2667c7ca1d47bab99b
natalie <webmistress@natalie.ee>
2019-07-14 12:52:15 -0400

    clean up batterymgr a bit

--- firmware/main/battery.c
+++ firmware/main/battery.c
@@ -5,32 +5,30 @@
 #include "esp_adc_cal.h"
 #include "esp_log.h"
 #include "pins.h"
+#include "ioexp.h"
 
 static const char* TAG = "BatteryMgr";
 static esp_adc_cal_characteristics_t *BatteryMgr_AdcChars;
+uint16_t BatteryMgr_Voltage = 0;
 
 bool BatteryMgr_Setup() {
     ESP_LOGI(TAG, "Setting up ADC");
     adc1_config_width(ADC_WIDTH_BIT_12);
     adc1_config_channel_atten(ADC_CHANNEL_6, ADC_ATTEN_DB_11);
     BatteryMgr_AdcChars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
-    /*esp_adc_cal_value_t type = */esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, BatteryMgr_AdcChars);
-    
-    ESP_LOGI(TAG, "Setting up sense control");
-    gpio_set_direction(PIN_BAT_SENSE_EN, GPIO_MODE_OUTPUT);
-    gpio_set_level(PIN_BAT_SENSE_EN, 0);
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_12, 1100, BatteryMgr_AdcChars);
 
     return true;
 }
 
 uint16_t BatteryMgr_Sample() {
-    gpio_set_level(PIN_BAT_SENSE_EN, 1);
+    IoExp_BatSenseControl(true);
     vTaskDelay(pdMS_TO_TICKS(5));
     uint32_t accu = 0;
     for (uint8_t i=0;i<100;i++) {
         accu += adc1_get_raw(ADC_CHANNEL_6);
     }
-    gpio_set_level(PIN_BAT_SENSE_EN, 0);
+    IoExp_BatSenseControl(false);
     return accu/100;
 }
 
@@ -39,6 +37,7 @@ void BatteryMgr_Main() {
     while (1) {
         uint32_t v = esp_adc_cal_raw_to_voltage(BatteryMgr_Sample(), BatteryMgr_AdcChars);
         v *= 2; //voltage divider on input
+        BatteryMgr_Voltage = v;
         ESP_LOGI(TAG, "bat %dmV", v);
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
--- firmware/main/battery.h
+++ firmware/main/battery.h
@@ -4,6 +4,8 @@
 #include "freertos/FreeRTOS.h"
 #include "esp_system.h"
 
+extern uint16_t BatteryMgr_Voltage;
+
 bool BatteryMgr_Setup();
 void BatteryMgr_Main();
 
