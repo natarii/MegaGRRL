484416663338e11042aeb5d556f52256169f2bb5
natalie <webmistress@natalie.ee>
2019-07-14 01:45:34 -0400

    add taskmgr

---
+++ firmware/main/taskmgr.c
@@ -0,0 +1,61 @@
+#include "taskmgr.h"
+#include "driver.h"
+#include "i2c.h"
+#include "ioexp.h"
+#include "battery.h"
+#include "key.h"
+#include "graphics.h"
+#include "dacstream.h"
+#include "lcddma.h"
+#include "loader.h"
+#include "player.h"
+#include "esp_log.h"
+#include "channels.h"
+#include "ui.h"
+
+static const char* TAG = "Taskmgr";
+
+TaskHandle_t Taskmgr_Handles[TASK_COUNT];
+
+void Taskmgr_Debug() {
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void Taskmgr_CreateTasks() {
+    for (uint8_t i=0;i<TASK_COUNT;i++) {
+        if (i == TASK_LCDDMA) continue;
+        Taskmgr_Handles[i] = NULL;
+    }
+    xTaskCreatePinnedToCore(BatteryMgr_Main, "BatteryMgr", 2048, NULL, 3, &Taskmgr_Handles[TASK_BATTERYMGR], 0);
+    xTaskCreatePinnedToCore(Loader_Main, "Loader", 4096, NULL, 10, &Taskmgr_Handles[TASK_LOADER], 0);
+    xTaskCreatePinnedToCore(IoExp_Main, "IoExp", 2048, NULL, 19, &Taskmgr_Handles[TASK_IOEXP], 0);
+    xTaskCreatePinnedToCore(KeyMgr_Main, "KeyMgr", 2048, NULL, 19, &Taskmgr_Handles[TASK_KEYMGR], 0);
+    xTaskCreatePinnedToCore(DacStream_FindTask, "DacStream_Find", 4096, NULL, 9, &Taskmgr_Handles[TASK_DACSTREAM_FIND], 0);
+    xTaskCreatePinnedToCore(DacStream_FillTask, "DacStream_Fill", 4096, NULL, 11, &Taskmgr_Handles[TASK_DACSTREAM_FILL], 0);
+    xTaskCreatePinnedToCore(Player_Main, "Player", 4096, NULL, 5, &Taskmgr_Handles[TASK_PLAYER], 0);
+    xTaskCreatePinnedToCore(ChannelMgr_Main, "ChannelMgr", 1024, NULL, 18, &Taskmgr_Handles[TASK_CHANNELMGR], 0);
+    xTaskCreatePinnedToCore(Ui_Main, "Ui", 2048, NULL, 5, &Taskmgr_Handles[TASK_UI], 0);
+
+    xTaskCreatePinnedToCore(Driver_Main, "Driver", 4096, NULL, configMAX_PRIORITIES-2, &Taskmgr_Handles[TASK_DRIVER], 1);
+}
+
+char buf[50*TASK_COUNT];
+void Taskmgr_Monitor() {
+    ESP_LOGI(TAG, "Task start");
+    while (1) {
+        ESP_LOGI(TAG, "Task stack high water mark follows");
+        for (uint8_t i=0;i<TASK_COUNT;i++) {
+            if (Taskmgr_Handles[i] != NULL) {
+                ESP_LOGI(TAG, "#%d: %d bytes free", i, uxTaskGetStackHighWaterMark(Taskmgr_Handles[i]));
+            } else {
+                ESP_LOGI(TAG, "#%d: not started", i);
+            }
+        }
+        ESP_LOGI(TAG, "Task runtime stats follow");
+        vTaskGetRunTimeStats((char*)buf);
+        printf(buf);
+        vTaskDelay(pdMS_TO_TICKS(3000));
+    }
+}
---
+++ firmware/main/taskmgr.h
@@ -0,0 +1,31 @@
+#ifndef AGR_TASKMGR_H
+#define AGR_TASKMGR_H
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_task_wdt.h"
+#include "esp_system.h"
+
+enum Tasks {
+    TASK_SELF,
+    TASK_DRIVER,
+    TASK_LOADER,
+    TASK_IOEXP,
+    TASK_BATTERYMGR,
+    TASK_KEYMGR,
+    TASK_DACSTREAM_FIND,
+    TASK_DACSTREAM_FILL,
+    TASK_PLAYER,
+    TASK_LCDDMA,
+    TASK_MONITOR,
+    TASK_CHANNELMGR,
+    TASK_UI,
+    TASK_COUNT
+};
+
+extern TaskHandle_t Taskmgr_Handles[TASK_COUNT];
+
+void Taskmgr_CreateTasks();
+void Taskmgr_Monitor();
+
+#endif
