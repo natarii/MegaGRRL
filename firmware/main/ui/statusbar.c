867f5353247deaba31bba4ccc03100ceea706cb4
natalie <webmistress@natalie.ee>
2019-07-15 00:13:02 -0400

    commit statusbar ui stuff - this is kinda jank

---
+++ firmware/main/ui/statusbar.c
@@ -0,0 +1,78 @@
+#include "freertos/FreeRTOS.h"
+#include "statusbar.h"
+#include "esp_log.h"
+#include "lvgl.h"
+#include "../lcddma.h"
+#include "../battery.h"
+#include "../ioexp.h"
+
+static const char* TAG = "Ui_StatusBar";
+
+lv_obj_t *container;
+lv_obj_t *charge;
+lv_obj_t *battery;
+lv_obj_t *voltage;
+char tempbuf[10];
+
+bool Ui_StatusBar_Setup(lv_obj_t *uiscreen) {
+    LcdDma_Mutex_Take(pdMS_TO_TICKS(1000));
+
+    container = lv_cont_create(uiscreen, NULL);
+
+    static lv_style_t style;
+    lv_style_copy(&style, &lv_style_plain);
+    style.body.main_color = LV_COLOR_MAKE(0,0,255);
+    style.body.grad_color = LV_COLOR_MAKE(0,127,0);
+    style.body.radius = 0;
+    style.text.font = &lv_font_dejavu_20;
+    style.text.color = LV_COLOR_MAKE(255,255,255);
+    lv_cont_set_style(container, &style);
+    lv_obj_set_height(container, 34);
+    lv_obj_set_width(container, 240);
+    lv_cont_set_fit(container, false, false);
+
+    charge = lv_label_create(container, NULL);
+    lv_obj_set_pos(charge, 2, 5);
+    lv_label_set_text(charge, SYMBOL_CHARGE);
+    lv_obj_set_width(charge, 24);
+
+    battery = lv_label_create(container, NULL);
+    lv_obj_set_pos(battery, 2+24+2, 5);
+    lv_label_set_text(battery, SYMBOL_BATTERY_3);
+    lv_obj_set_width(charge, 24);
+
+    voltage = lv_label_create(container, NULL);
+    lv_obj_set_pos(voltage, 2+24+2+24+2+5/*?*/, 5);
+    lv_label_set_text(voltage, "????mV");
+    lv_obj_set_width(voltage, 100);
+
+    LcdDma_Mutex_Give();
+    return true;
+}
+
+uint32_t last = 0;
+void Ui_StatusBar_Tick() {
+    if (xthal_get_ccount() - last >= 240000000) {
+        LcdDma_Mutex_Take(pdMS_TO_TICKS(1000));
+        if (IoExp_ChargeStatus()) {
+            lv_label_set_text(charge, SYMBOL_CHARGE);
+        } else {
+            lv_label_set_text(charge, "");
+        }
+        if (BatteryMgr_Voltage >= 4050) {
+            lv_label_set_text(battery, SYMBOL_BATTERY_FULL);
+        } else if (BatteryMgr_Voltage >= 3850) {
+            lv_label_set_text(battery, SYMBOL_BATTERY_3);
+        } else if (BatteryMgr_Voltage >= 3600) {
+            lv_label_set_text(battery, SYMBOL_BATTERY_2);
+        } else if (BatteryMgr_Voltage >= 3350) {
+            lv_label_set_text(battery, SYMBOL_BATTERY_1);
+        } else {
+            lv_label_set_text(battery, SYMBOL_BATTERY_EMPTY);
+        }
+        sprintf(tempbuf, "%dmV", BatteryMgr_Voltage);
+        lv_label_set_text(voltage, tempbuf);
+        LcdDma_Mutex_Give();
+        last = xthal_get_ccount();
+    }
+}
---
+++ firmware/main/ui/statusbar.h
@@ -0,0 +1,10 @@
+#ifndef AGR_UI_STATUSBAR_H
+#define AGR_UI_STATUSBAR_H
+
+#include "esp_system.h"
+#include "lvgl.h"
+
+bool Ui_StatusBar_Setup(lv_obj_t *uiscreen);
+void Ui_StatusBar_Tick();
+
+#endif
