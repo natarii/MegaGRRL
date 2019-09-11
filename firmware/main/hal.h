5eafb4c50cdb4d383ae5281acad1c40537efe65e
natalie <webmistress@natalie.ee>
2019-09-11 03:25:38 -0400

    start on hal stuff

---
+++ firmware/main/hal.h
@@ -0,0 +1,13 @@
+#ifndef AGR_HAL_H
+#define AGR_HAL_H
+
+//MegaGRRL hardware defs
+//this needs to be updated based on which hardware version is being targeted in the build.
+
+//MegaGRRL Portable, all supported revisions
+#define HWVER_PORTABLE
+
+//MegaGRRL Desktop, all supported revisions
+//#define HWVER_DESKTOP
+
+#endif
