f680e4cef9f90f5a7a01a3274855fd8ad6df5691
natalie <webmistress@natalie.ee>
2019-09-19 23:35:24 -0400

    update hal

--- firmware/main/hal.h
+++ firmware/main/hal.h
@@ -4,10 +4,10 @@
 //MegaGRRL hardware defs
 //this needs to be updated based on which hardware version is being targeted in the build.
 
-//MegaGRRL Portable, all supported revisions
+//MegaGRRL Portable, ver 2.1
 #define HWVER_PORTABLE
 
-//MegaGRRL Desktop, all supported revisions
+//MegaGRRL Desktop, ver 1.0
 //#define HWVER_DESKTOP
 
 #endif
