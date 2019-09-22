312ac24dc1697407c8e847bdd43e6adcbbb0c076
natalie <webmistress@natalie.ee>
2019-09-21 21:05:33 -0400

    commit all unstaged player updates

--- firmware/main/player.c
+++ firmware/main/player.c
@@ -5,6 +5,13 @@
 #include "ioexp.h"
 #include "loader.h"
 #include "dacstream.h"
+#include "freertos/event_groups.h"
+#include "clk.h"
+#include "queue.h"
+#include "vgm.h"
+#include "gd3.h"
+#include "mallocs.h"
+#include "ui/nowplaying.h"
 
 static const char* TAG = "Player";
 
@@ -14,18 +21,164 @@ FILE *Player_DsFindFile;
 FILE *Player_DsFillFile;
 VgmInfoStruct_t Player_Info;
 uint32_t notif = 0;
+
+volatile uint8_t Player_SetLoopCount = 2;
+volatile uint8_t Player_LoopCount = 2;
+volatile RepeatMode_t Player_RepeatMode = REPEAT_ALL;
+
+EventGroupHandle_t Player_Status;
+StaticEventGroup_t Player_StatusBuf;
+
+char Player_Gd3_Title[PLAYER_GD3_FIELD_SIZES+1];
+char Player_Gd3_Game[PLAYER_GD3_FIELD_SIZES+1];
+char Player_Gd3_Author[PLAYER_GD3_FIELD_SIZES+1];
+
 bool stopped = true;
+
+bool Player_NextTrk(bool UserSpecified) { //returns true if there is now a track playing
+    Player_StopTrack();
+    if (!UserSpecified && Player_RepeatMode == REPEAT_ONE) {
+        //nothing to do - just start the same track again
+    } else {
+        bool end = (QueuePosition == QueueLength-1);
+        if (end) {
+            if (Player_RepeatMode == REPEAT_NONE) {
+                return false; //nothing more to play
+            } else if (Player_RepeatMode == REPEAT_ALL) {
+                QueuePosition = 0;
+                QueueSetupEntry(false);
+            } else if (Player_RepeatMode == REPEAT_ONE) { //if it's repeat one and they manually pressed next
+                return false; //nothing more to play
+            }
+        } else { //not the end of the queue, just load the next track
+            QueueNext();
+            QueueSetupEntry(false);
+        }
+    }
+    Player_StartTrack(&QueuePlayingFilename[0]);
+    return true;
+}
+
+bool Player_PrevTrk(bool UserSpecified) { //returns true if there is now a track playing
+    Player_StopTrack();
+    if (!UserSpecified && Player_RepeatMode == REPEAT_ONE) {
+        //nothing to do - just start the same track again
+    } else {
+        bool end = (QueuePosition == 0);
+        if (end) {
+            if (Player_RepeatMode == REPEAT_NONE) {
+                return false; //nothing more to play
+            } else if (Player_RepeatMode == REPEAT_ALL) {
+                QueuePosition = QueueLength-1;
+                QueueSetupEntry(false);
+            } else if (Player_RepeatMode == REPEAT_ONE) { //if it's repeat one and they manually pressed prev
+                return false; //nothing more to play
+            }
+        } else { //not the end of the queue, just load the prev track
+            QueuePrev();
+            QueueSetupEntry(false);
+        }
+    }
+    Player_StartTrack(&QueuePlayingFilename[0]);
+    return true;
+}
 
 void Player_Main() {
     ESP_LOGI(TAG, "Task start");
 
     while (1) {
-        if (xTaskNotifyWait(0,0xffffffff, &notif, pdMS_TO_TICKS(3000)) == pdTRUE) {
-            if (notif == PLAYER_NOTIFY_PLAY) {
-                
+        if (xTaskNotifyWait(0,0xffffffff, &notif, pdMS_TO_TICKS(250)) == pdTRUE) {
+            if (notif == PLAYER_NOTIFY_START_RUNNING) {
+                xEventGroupClearBits(Player_Status, PLAYER_STATUS_NOT_RUNNING);
+                xEventGroupClearBits(Player_Status, PLAYER_STATUS_PAUSED);
+                if ((xEventGroupGetBits(Player_Status) & PLAYER_STATUS_RUNNING) == 0) {
+                    xEventGroupSetBits(Player_Status, PLAYER_STATUS_RUNNING); //do this now, Player_StartTrack could take longer than the timeout of the task waiting for this event.
+                    QueueSetupEntry(false);
+                    Player_StartTrack(&QueuePlayingFilename[0]);
+                } else {
+                    //already running. yikes!
+                }
+            } else if (notif == PLAYER_NOTIFY_STOP_RUNNING) {
+                if (xEventGroupGetBits(Player_Status) & PLAYER_STATUS_RUNNING) {
+                    Player_StopTrack();
+                }
+                xEventGroupClearBits(Player_Status, PLAYER_STATUS_RUNNING);
+                xEventGroupSetBits(Player_Status, PLAYER_STATUS_NOT_RUNNING);
+            } else if (notif == PLAYER_NOTIFY_NEXT) {
+                xEventGroupClearBits(Player_Status, PLAYER_STATUS_PAUSED);
+                if (Player_NextTrk(true)) {
+                    //nothing to do, i don't think...
+                } else {
+                    xEventGroupClearBits(Player_Status, PLAYER_STATUS_RUNNING);
+                    xEventGroupSetBits(Player_Status, PLAYER_STATUS_NOT_RUNNING);
+                }
+            } else if (notif == PLAYER_NOTIFY_PREV) {
+                xEventGroupClearBits(Player_Status, PLAYER_STATUS_PAUSED);
+                if (Driver_Sample < 3*44100) { //actually change track
+                    if (Player_PrevTrk(true)) {
+                        //nothing to do, i don't think...
+                    } else {
+                        xEventGroupClearBits(Player_Status, PLAYER_STATUS_RUNNING);
+                        xEventGroupSetBits(Player_Status, PLAYER_STATUS_NOT_RUNNING);
+                    }
+                } else { //just restart
+                    Player_StopTrack();
+                    Player_StartTrack(&QueuePlayingFilename[0]);
+                }
+            } else if (notif == PLAYER_NOTIFY_PAUSE) {
+                if (xEventGroupGetBits(Driver_CommandEvents) & DRIVER_EVENT_RUNNING) {
+                    ESP_LOGI(TAG, "Request driver stop...");
+                    xEventGroupSetBits(Driver_CommandEvents, DRIVER_EVENT_STOP_REQUEST);
+                    /*ESP_LOGI(TAG, "Wait for driver to stop...");
+                    if (xEventGroupWaitBits(Driver_CommandEvents, DRIVER_EVENT_RUNNING, false, false, pdMS_TO_TICKS(3000)) & DRIVER_EVENT_RUNNING) {
+                        ESP_LOGE(TAG, "Driver stop timeout !!");
+                        return false;
+                    }*/
+                }
+                xEventGroupSetBits(Player_Status, PLAYER_STATUS_PAUSED);
+            } else if (notif == PLAYER_NOTIFY_PLAY) {
+                if ((xEventGroupGetBits(Driver_CommandEvents) & DRIVER_EVENT_RUNNING) == 0) {
+                    ESP_LOGI(TAG, "Request driver resume...");
+                    xEventGroupSetBits(Driver_CommandEvents, DRIVER_EVENT_RESUME_REQUEST);
+                    ESP_LOGI(TAG, "Wait for driver to resume...");
+                    if ((xEventGroupWaitBits(Driver_CommandEvents, DRIVER_EVENT_RUNNING, false, false, pdMS_TO_TICKS(3000)) & DRIVER_EVENT_RUNNING) == 0) {
+                        ESP_LOGE(TAG, "Driver resume timeout !!");
+                        return false;
+                    }
+                }
+                xEventGroupClearBits(Player_Status, PLAYER_STATUS_PAUSED);
             }
+        } else { //no incoming notification
+
+        }
+        if ((xEventGroupGetBits(Player_Status) & PLAYER_STATUS_RUNNING) && (xEventGroupGetBits(Driver_CommandEvents) & DRIVER_EVENT_FINISHED)) { //still running, but driver reached end
+            ESP_LOGI(TAG, "Driver finished, starting next track");
+            xEventGroupClearBits(Player_Status, PLAYER_STATUS_PAUSED);
+            if (Player_NextTrk(false)) {
+                    //nothing to do, i don't think...
+                } else {
+                    xEventGroupClearBits(Player_Status, PLAYER_STATUS_RUNNING);
+                    xEventGroupSetBits(Player_Status, PLAYER_STATUS_NOT_RUNNING);
+                }
         }
     }
+}
+
+bool Player_Setup() {
+    ESP_LOGI(TAG, "Setting up");
+
+    Player_Gd3_Title[0] = 0;
+    Player_Gd3_Game[0] = 0;
+    Player_Gd3_Author[0] = 0;
+
+    ESP_LOGI(TAG, "Creating status event group");
+    Player_Status = xEventGroupCreateStatic(&Player_StatusBuf);
+    if (Player_Status == NULL) {
+        ESP_LOGE(TAG, "Failed !!");
+        return false;
+    }
+    xEventGroupSetBits(Player_Status, PLAYER_STATUS_NOT_RUNNING);
+    return true;
 }
 
 bool Player_StartTrack(char *FilePath) {
@@ -37,10 +190,45 @@ bool Player_StartTrack(char *FilePath) {
     fseek(Player_DsFindFile, 0, SEEK_SET);
     VgmParseHeader(Player_VgmFile, &Player_Info);
 
+    Gd3Descriptor_t desc;
+    Gd3ParseDescriptor(Player_VgmFile, &Player_Info, &desc);
+    if (desc.parsed) {
+        Gd3GetStringChars(Player_VgmFile, &desc, GD3STRING_TRACK_EN, &Player_Gd3_Title[0], PLAYER_GD3_FIELD_SIZES);
+        Gd3GetStringChars(Player_VgmFile, &desc, GD3STRING_GAME_EN, &Player_Gd3_Game[0], PLAYER_GD3_FIELD_SIZES);
+        Gd3GetStringChars(Player_VgmFile, &desc, GD3STRING_AUTHOR_EN, &Player_Gd3_Author[0], PLAYER_GD3_FIELD_SIZES);
+    } else {
+        Player_Gd3_Title[0] = 0;
+        Player_Gd3_Game[0] = 0;
+        Player_Gd3_Author[0] = 0;
+    }
+
+    Ui_NowPlaying_DataAvail = true;
+    Ui_NowPlaying_NewTrack = true;
+
     /* todo here:
     * set driver clock rate for wait scaling
-    * manage clkgens, add clamping
     */
+
+    ESP_LOGI(TAG, "vgm rate: %d", Player_Info.Rate);
+
+    uint32_t PsgClock = 0;
+    uint32_t FmClock = 0;
+    fseek(Player_VgmFile, 0x0c, SEEK_SET);
+    fread(&PsgClock, 4, 1, Player_VgmFile);
+    fseek(Player_VgmFile, 0x2c, SEEK_SET);
+    fread(&FmClock, 4, 1, Player_VgmFile);
+    ESP_LOGI(TAG, "Clocks from vgm: psg %d, fm %d", PsgClock, FmClock);
+
+    if (PsgClock == 0) PsgClock = 3579545;
+    else if (PsgClock < 3000000) PsgClock = 3000000;
+    else if (PsgClock > 4100000) PsgClock = 4100000;
+    if (FmClock == 0) FmClock = 7670453;
+    else if (FmClock < 7000000) FmClock = 7000000;
+    else if (FmClock > 8300000) FmClock = 8300000;
+    ESP_LOGI(TAG, "Clocks clamped: psg %d, fm %d", PsgClock, FmClock);
+    ESP_LOGI(TAG, "Enabling clocks");
+    Clk_Set(CLK_FM, FmClock);
+    Clk_Set(CLK_PSG, PsgClock);
 
     ESP_LOGI(TAG, "Signalling driver reset");
     xEventGroupSetBits(Driver_CommandEvents, DRIVER_EVENT_RESET_REQUEST);
@@ -106,6 +294,8 @@ bool Player_StartTrack(char *FilePath) {
 }
 
 bool Player_StopTrack() {
+    Ui_NowPlaying_DataAvail = false;
+
     ESP_LOGI(TAG, "Requesting driver stop...");
     xEventGroupSetBits(Driver_CommandEvents, DRIVER_EVENT_STOP_REQUEST);
 
@@ -116,6 +306,10 @@ bool Player_StopTrack() {
     bool ret = Loader_Stop();
     if (!ret) {
         ESP_LOGE(TAG, "Loader stop timeout !!");
+        fclose(Player_VgmFile);
+        fclose(Player_PcmFile);
+        fclose(Player_DsFindFile);
+        fclose(Player_DsFillFile);
         return false;
     }
 
@@ -123,6 +317,10 @@ bool Player_StopTrack() {
     ret = DacStream_Stop();
     if (!ret) {
         ESP_LOGE(TAG, "Dacstream stop timeout !!");
+        fclose(Player_VgmFile);
+        fclose(Player_PcmFile);
+        fclose(Player_DsFindFile);
+        fclose(Player_DsFillFile);
         return false;
     }
 
--- firmware/main/player.h
+++ firmware/main/player.h
@@ -2,19 +2,46 @@
 #define AGR_PLAYER_H
 
 #include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
 #include "esp_system.h"
 #include "unistd.h"
+#include "mallocs.h"
+#include "vgm.h"
+
+typedef enum {
+    REPEAT_NONE,
+    REPEAT_ONE,
+    REPEAT_ALL,
+    REPEAT_COUNT
+} RepeatMode_t;
 
 enum {
-    PLAYER_NOTIFY_PLAY = 1,
+    PLAYER_NOTIFY_START_RUNNING = 1,
+    PLAYER_NOTIFY_STOP_RUNNING,
+    PLAYER_NOTIFY_PLAY,
     PLAYER_NOTIFY_PAUSE,
     PLAYER_NOTIFY_STOP,
     PLAYER_NOTIFY_PREV,
     PLAYER_NOTIFY_NEXT,
+    PLAYER_NOTIFY_TRACK_DONE,
 };
+
+#define PLAYER_STATUS_NOT_RUNNING   0x01
+#define PLAYER_STATUS_RUNNING       0x02
+#define PLAYER_STATUS_PLAYING       0x04
+#define PLAYER_STATUS_PAUSED        0x08
+
+extern EventGroupHandle_t Player_Status;
+extern char Player_Gd3_Title[PLAYER_GD3_FIELD_SIZES+1];
+extern char Player_Gd3_Game[PLAYER_GD3_FIELD_SIZES+1];
+extern char Player_Gd3_Author[PLAYER_GD3_FIELD_SIZES+1];
+extern VgmInfoStruct_t Player_Info;
+extern volatile uint8_t Player_LoopCount;
+extern uint8_t Player_SetLoopCount;
 
 bool Player_StartTrack(char *FilePath);
 bool Player_StopTrack();
 void Player_Main();
+bool Player_Setup();
 
 #endif
