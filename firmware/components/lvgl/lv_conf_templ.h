904134a5db062f7f1aa5f73275659ac1b51a25fd
natalie <webmistress@natalie.ee>
2019-10-03 00:04:16 -0400

    readd lvgl with updated config

--- firmware/components/lvgl/lv_conf_templ.h
+++ firmware/components/lvgl/lv_conf_templ.h
@@ -7,7 +7,7 @@
  * COPY THIS FILE AS lv_conf.h
  */
 
-#if 1 /*Set it to "1" to enable content*/
+#if 0 /*Set it to "1" to enable content*/
 
 #ifndef LV_CONF_H
 #define LV_CONF_H
@@ -18,15 +18,15 @@
 
 /* Memory size which will be used by the library
  * to store the graphical objects and other data */
-#define LV_MEM_CUSTOM      1                /*1: use custom malloc/free, 0: use the built-in lv_mem_alloc/lv_mem_free*/
+#define LV_MEM_CUSTOM      0                /*1: use custom malloc/free, 0: use the built-in lv_mem_alloc/lv_mem_free*/
 #if LV_MEM_CUSTOM == 0
 #define LV_MEM_SIZE    (32U * 1024U)        /*Size memory used by `lv_mem_alloc` in bytes (>= 2kB)*/
 #define LV_MEM_ATTR                         /*Complier prefix for big array declaration*/
 #define LV_MEM_AUTO_DEFRAG  1               /*Automatically defrag on free*/
 #else       /*LV_MEM_CUSTOM*/
-#define LV_MEM_CUSTOM_INCLUDE "freertos/FreeRTOS.h"   /*Header for the dynamic memory function*/
-#define LV_MEM_CUSTOM_ALLOC   pvPortMalloc       /*Wrapper to malloc*/
-#define LV_MEM_CUSTOM_FREE    vPortFree         /*Wrapper to free*/
+#define LV_MEM_CUSTOM_INCLUDE <stdlib.h>   /*Header for the dynamic memory function*/
+#define LV_MEM_CUSTOM_ALLOC   malloc       /*Wrapper to malloc*/
+#define LV_MEM_CUSTOM_FREE    free         /*Wrapper to free*/
 #endif     /*LV_MEM_CUSTOM*/
 
 /*===================
@@ -34,15 +34,15 @@
  *===================*/
 
 /* Horizontal and vertical resolution of the library.*/
-#define LV_HOR_RES          (240)
-#define LV_VER_RES          (320)
-#define LV_DPI              166
+#define LV_HOR_RES          (320)
+#define LV_VER_RES          (240)
+#define LV_DPI              100
 
 /* Size of VDB (Virtual Display Buffer: the internal graphics buffer).
  * Required for buffered drawing, opacity and anti-aliasing
  * VDB makes the double buffering, you don't need to deal with it!
  * Typical size: ~1/10 screen */
-#define LV_VDB_SIZE         4094/2  /*Size of VDB in pixel count (1/10 screen size is good for first)*/
+#define LV_VDB_SIZE         (30 * LV_HOR_RES)  /*Size of VDB in pixel count (1/10 screen size is good for first)*/
 #define LV_VDB_PX_BPP       LV_COLOR_SIZE      /*Bit-per-pixel of VDB. Useful for monochrome or non-standard color format displays. (Special formats are handled with `disp_drv->vdb_wr`)*/
 #define LV_VDB_ADR          0                  /*Place VDB to a specific address (e.g. in external RAM) (0: allocate automatically into RAM; LV_VDB_ADR_INV: to replace it later with `lv_vdb_set_adr()`)*/
 
@@ -72,21 +72,21 @@
 
 /*Color settings*/
 #define LV_COLOR_DEPTH     16                     /*Color depth: 1/8/16/32*/
-#define LV_COLOR_16_SWAP   1                      /*Swap the 2 bytes of RGB565 color. Useful if the display has a 8 bit interface (e.g. SPI)*/
+#define LV_COLOR_16_SWAP   0                      /*Swap the 2 bytes of RGB565 color. Useful if the display has a 8 bit interface (e.g. SPI)*/
 #define LV_COLOR_SCREEN_TRANSP        0           /*1: Enable screen transparency. Useful for OSD or other overlapping GUIs. Requires ARGB8888 colors*/
-#define LV_COLOR_TRANSP    LV_COLOR_MAKE(0x12,0x34,0x56)          /*Images pixels with this color will not be drawn (with chroma keying)*/
+#define LV_COLOR_TRANSP    LV_COLOR_LIME          /*Images pixels with this color will not be drawn (with chroma keying)*/
 
 /*Text settings*/
-#define LV_TXT_UTF8             0                /*Enable UTF-8 coded Unicode character usage */
+#define LV_TXT_UTF8             1                /*Enable UTF-8 coded Unicode character usage */
 #define LV_TXT_BREAK_CHARS     " ,.;:-_"         /*Can break texts on these chars*/
 
 /*Graphics feature usage*/
 #define USE_LV_ANIMATION        1               /*1: Enable all animations*/
-#define USE_LV_SHADOW           0               /*1: Enable shadows*/
-#define USE_LV_GROUP            0               /*1: Enable object groups (for keyboards)*/
-#define USE_LV_GPU              0               /*1: Enable GPU interface*/
-#define USE_LV_REAL_DRAW        0               /*1: Enable function which draw directly to the frame buffer instead of VDB (required if LV_VDB_SIZE = 0)*/
-#define USE_LV_FILESYSTEM       0               /*1: Enable file system (required by images*/
+#define USE_LV_SHADOW           1               /*1: Enable shadows*/
+#define USE_LV_GROUP            1               /*1: Enable object groups (for keyboards)*/
+#define USE_LV_GPU              1               /*1: Enable GPU interface*/
+#define USE_LV_REAL_DRAW        1               /*1: Enable function which draw directly to the frame buffer instead of VDB (required if LV_VDB_SIZE = 0)*/
+#define USE_LV_FILESYSTEM       1               /*1: Enable file system (required by images*/
 
 /*Compiler settings*/
 #define LV_ATTRIBUTE_TICK_INC                   /* Define a custom attribute to `lv_tick_inc` function */
@@ -103,7 +103,7 @@
 
 
 /*Log settings*/
-#define USE_LV_LOG      0   /*Enable/disable the log module*/
+#define USE_LV_LOG      1   /*Enable/disable the log module*/
 #if USE_LV_LOG
 /* How important log should be added:
  * LV_LOG_LEVEL_TRACE       A lot of logs to give detailed information
@@ -138,7 +138,7 @@
 /* More info about fonts: https://littlevgl.com/basics#fonts
  * To enable a built-in font use 1,2,4 or 8 values
  * which will determine the bit-per-pixel */
-#define USE_LV_FONT_DEJAVU_10              1
+#define USE_LV_FONT_DEJAVU_10              0
 #define USE_LV_FONT_DEJAVU_10_LATIN_SUP    0
 #define USE_LV_FONT_DEJAVU_10_CYRILLIC     0
 #define USE_LV_FONT_SYMBOL_10              0
@@ -146,7 +146,7 @@
 #define USE_LV_FONT_DEJAVU_20              4
 #define USE_LV_FONT_DEJAVU_20_LATIN_SUP    0
 #define USE_LV_FONT_DEJAVU_20_CYRILLIC     0
-#define USE_LV_FONT_SYMBOL_20              1
+#define USE_LV_FONT_SYMBOL_20              4
 
 #define USE_LV_FONT_DEJAVU_30              0
 #define USE_LV_FONT_DEJAVU_30_LATIN_SUP    0
@@ -158,7 +158,7 @@
 #define USE_LV_FONT_DEJAVU_40_CYRILLIC     0
 #define USE_LV_FONT_SYMBOL_40              0
 
-#define USE_LV_FONT_MONOSPACE_8            1
+#define USE_LV_FONT_MONOSPACE_8            0
 
 /* Optionally declare your custom fonts here.
  * You can use these fonts as default font too
@@ -168,7 +168,7 @@
  */
 #define LV_FONT_CUSTOM_DECLARE
 
-#define LV_FONT_DEFAULT        &lv_font_dejavu_10     /*Always set a default font from the built-in fonts*/
+#define LV_FONT_DEFAULT        &lv_font_dejavu_20     /*Always set a default font from the built-in fonts*/
 
 /*===================
  *  LV_OBJ SETTINGS
@@ -204,7 +204,7 @@
 #define USE_LV_LINE     1
 
 /*Arc (dependencies: -)*/
-#define USE_LV_ARC      0
+#define USE_LV_ARC      1
 
 /*******************
  * Container objects
@@ -217,10 +217,10 @@
 #define USE_LV_PAGE     1
 
 /*Window (dependencies: lv_cont, lv_btn, lv_label, lv_img, lv_page)*/
-#define USE_LV_WIN      0
+#define USE_LV_WIN      1
 
 /*Tab (dependencies: lv_page, lv_btnm)*/
-#define USE_LV_TABVIEW      0
+#define USE_LV_TABVIEW      1
 #if USE_LV_TABVIEW != 0
 #define LV_TABVIEW_ANIM_TIME    300     /*Time of slide animation [ms] (0: no animation)*/
 #endif
@@ -230,22 +230,22 @@
  *************************/
 
 /*Bar (dependencies: -)*/
-#define USE_LV_BAR      0
+#define USE_LV_BAR      1
 
 /*Line meter (dependencies: *;)*/
-#define USE_LV_LMETER   0
+#define USE_LV_LMETER   1
 
 /*Gauge (dependencies:bar, lmeter)*/
-#define USE_LV_GAUGE    0
+#define USE_LV_GAUGE    1
 
 /*Chart (dependencies: -)*/
-#define USE_LV_CHART    0
+#define USE_LV_CHART    1
 
 /*LED (dependencies: -)*/
-#define USE_LV_LED      0
+#define USE_LV_LED      1
 
 /*Message box (dependencies: lv_rect, lv_btnm, lv_label)*/
-#define USE_LV_MBOX     0
+#define USE_LV_MBOX     1
 
 /*Text area (dependencies: lv_label, lv_page)*/
 #define USE_LV_TA       1
@@ -255,10 +255,10 @@
 #endif
 
 /*Calendar (dependencies: -)*/
-#define USE_LV_CALENDAR 0
+#define USE_LV_CALENDAR 1
 
 /*Preload (dependencies: arc)*/
-#define USE_LV_PRELOAD      0
+#define USE_LV_PRELOAD      1
 #if USE_LV_PRELOAD != 0
 #define LV_PRELOAD_DEF_ARC_LENGTH   60      /*[deg]*/
 #define LV_PRELOAD_DEF_SPIN_TIME    1000    /*[ms]*/
@@ -269,46 +269,46 @@
  *************************/
 
 /*Button (dependencies: lv_cont*/
-#define USE_LV_BTN      0
+#define USE_LV_BTN      1
 #if USE_LV_BTN != 0
 #define LV_BTN_INK_EFFECT   1       /*Enable button-state animations - draw a circle on click (dependencies: USE_LV_ANIMATION)*/
 #endif
 
 /*Image Button (dependencies: lv_btn*/
-#define USE_LV_IMGBTN   0
+#define USE_LV_IMGBTN   1
 
 /*Button matrix (dependencies: -)*/
-#define USE_LV_BTNM     0
+#define USE_LV_BTNM     1
 
 /*Keyboard (dependencies: lv_btnm)*/
-#define USE_LV_KB       0
+#define USE_LV_KB       1
 
 /*Check box (dependencies: lv_btn, lv_label)*/
-#define USE_LV_CB       0
+#define USE_LV_CB       1
 
 /*List (dependencies: lv_page, lv_btn, lv_label, (lv_img optionally for icons ))*/
-#define USE_LV_LIST     0
+#define USE_LV_LIST     1
 #if USE_LV_LIST != 0
 #define LV_LIST_FOCUS_TIME  100 /*Default animation time of focusing to a list element [ms] (0: no animation)  */
 #endif
 
 /*Drop down list (dependencies: lv_page, lv_label)*/
-#define USE_LV_DDLIST    0
+#define USE_LV_DDLIST    1
 #if USE_LV_DDLIST != 0
 #define LV_DDLIST_ANIM_TIME     200     /*Open and close default animation time [ms] (0: no animation)*/
 #endif
 
 /*Roller (dependencies: lv_ddlist)*/
-#define USE_LV_ROLLER    0
+#define USE_LV_ROLLER    1
 #if USE_LV_ROLLER != 0
 #define LV_ROLLER_ANIM_TIME     200     /*Focus animation time [ms] (0: no animation)*/
 #endif
 
 /*Slider (dependencies: lv_bar)*/
-#define USE_LV_SLIDER    0
+#define USE_LV_SLIDER    1
 
 /*Switch (dependencies: lv_slider)*/
-#define USE_LV_SW       0
+#define USE_LV_SW       1
 
 /*************************
  * Non-user section
