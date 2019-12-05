663dff7e387879814e2a52e50da354bcd22af03b
natalie <webmistress@natalie.ee>
2019-12-04 22:30:14 -0500

    just switch hal to desktop by default for now

--- firmware/main/hal.h
+++ firmware/main/hal.h
@@ -13,9 +13,9 @@
 //this needs to be updated based on which hardware version is being targeted in the build.
 
 //MegaGRRL Portable, ver 2.1
-#define HWVER_PORTABLE
+//#define HWVER_PORTABLE
 
 //MegaGRRL Desktop, ver 1.0
-//#define HWVER_DESKTOP
+#define HWVER_DESKTOP
 
 #endif
