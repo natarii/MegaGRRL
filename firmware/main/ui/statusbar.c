bc2349a60d70ea7ef942b44d1497d209c793ef33
natalie <webmistress@natalie.ee>
2020-10-14 06:10:55 -0400

    Add shuffle icon to statusbar

--- firmware/main/ui/statusbar.c
+++ firmware/main/ui/statusbar.c
@@ -9,6 +9,7 @@
 #include "freertos/event_groups.h"
 #include "../driver.h"
 #include "../player.h"
+#include "../queue.h"
 
 static const char* TAG = "Ui_StatusBar";
 
@@ -29,9 +30,10 @@ typedef enum {
     ICON_STEREO,
     ICON_SOUND,
     ICON_EXTRACT,
-    ICON_REPEAT_NONE,
+    ICON_BLANK,
     ICON_REPEAT_ONE,
     ICON_REPEAT_ALL,
+    ICON_SHUFFLE,
     ICON_COUNT
 } StatusbarIcon_t;
 
@@ -53,9 +55,10 @@ const StatusbarIconDef_t icondefs[] = {
     {"", 0, 0},
     {"1", 0, 0},
     {"A", 0, 0},
+    {LV_SYMBOL_SHUFFLE, 0, 0},
 };
 
-static IRAM_ATTR lv_obj_t *iconlabels[5];
+static IRAM_ATTR lv_obj_t *iconlabels[6];
 
 void Ui_StatusBar_DrawIcon(StatusbarIcon_t icon, lv_obj_t *label, int16_t x, int16_t y) {
     lv_label_set_static_text(label, icondefs[icon].c);
@@ -79,7 +82,7 @@ bool Ui_StatusBar_Setup(lv_obj_t *uiscreen) {
     lv_obj_set_width(container, 240);
     //lv_cont_set_fit(container, false, false);
 
-    for (uint8_t i=0;i<5;i++) {
+    for (uint8_t i=0;i<6;i++) {
         iconlabels[i] = lv_label_create(container, NULL);
         lv_label_set_static_text(iconlabels[i], "");
     }
@@ -122,7 +125,7 @@ void Ui_StatusBar_RedrawIcons() {
 
     switch (Player_RepeatMode) {
         case REPEAT_NONE:
-            Ui_StatusBar_DrawIcon(ICON_REPEAT_NONE, iconlabels[4], 83, 7);
+            Ui_StatusBar_DrawIcon(ICON_BLANK, iconlabels[4], 83, 7); //0px wide...
             break;
         case REPEAT_ONE:
             Ui_StatusBar_DrawIcon(ICON_REPEAT_ONE, iconlabels[4], 83, 7);
@@ -134,12 +137,19 @@ void Ui_StatusBar_RedrawIcons() {
             //oh god oh fuck
             break;
     }
+
+    uint16_t nextx = 83;
+    if (Player_RepeatMode != REPEAT_NONE) nextx += 17;
+    Ui_StatusBar_DrawIcon(Queue_Shuffle?ICON_SHUFFLE:ICON_BLANK, iconlabels[5], nextx, 7);
 }
 
 void Ui_StatusBar_SetExtract(bool extracting) {
     LcdDma_Mutex_Take(pdMS_TO_TICKS(1000));
     if (extracting) {
-        Ui_StatusBar_DrawIcon(ICON_EXTRACT, iconlabels[3], (Player_RepeatMode==REPEAT_NONE)?83:100, 7);
+        uint16_t nextx = 83;
+        if (Player_RepeatMode != REPEAT_NONE) nextx += 17;
+        if (Queue_Shuffle) nextx += 17;
+        Ui_StatusBar_DrawIcon(ICON_EXTRACT, iconlabels[3], nextx, 7);
     } else {
         lv_label_set_static_text(iconlabels[3], "");
     }
@@ -151,6 +161,7 @@ EventBits_t lastplayerevents = 0xff;
 bool lastforcemono = true;
 uint8_t statusbarmasks[2] = {0xff, 0xff};
 uint8_t lastrepeatmode = 0xff;
+bool lastshuffle = false;
 void Ui_StatusBar_Tick() {
     if (xthal_get_ccount() - last >= 24000000) {
         EventBits_t playerevents = xEventGroupGetBits(Player_Status);
@@ -166,6 +177,9 @@ void Ui_StatusBar_Tick() {
             redraw = true;
         }
         if (lastrepeatmode != Player_RepeatMode) {
+            redraw = true;
+        }
+        if (lastshuffle != Queue_Shuffle) {
             redraw = true;
         }
         if (redraw) Ui_StatusBar_RedrawIcons();
@@ -193,6 +207,7 @@ void Ui_StatusBar_Tick() {
         statusbarmasks[0] = Driver_FmMask;
         statusbarmasks[1] = Driver_PsgMask;
         lastrepeatmode = Player_RepeatMode;
+        lastshuffle = Queue_Shuffle;
         last = xthal_get_ccount();
     }
 }
