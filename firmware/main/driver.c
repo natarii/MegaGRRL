6c26139f0ec915af9e1ae04e835965a6147f23dd
natalie <webmistress@natalie.ee>
2021-03-20 00:31:41 -0400

    Force OPN2 test registers to first bank

--- firmware/main/driver.c
+++ firmware/main/driver.c
@@ -545,6 +545,7 @@ void Driver_FmOut(uint8_t Port, uint8_t Register, uint8_t Value) {
     //these regs can be written to either bank. for dedup purposes, force them all to the first bank.
     if (Port) {
         switch (Register) {
+            case 0x21:
             case 0x22:
             case 0x24:
             case 0x25:
@@ -553,6 +554,7 @@ void Driver_FmOut(uint8_t Port, uint8_t Register, uint8_t Value) {
             case 0x28:
             case 0x2a:
             case 0x2b:
+            case 0x2c:
                 Port = 0;
                 break;
             default:
