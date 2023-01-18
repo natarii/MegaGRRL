e137a7f9d721600270c7ba913786be7e52d5a8dd
natalie <webmistress@natalie.ee>
2023-01-18 07:06:46 -0500

    add natutils

---
+++ firmware/components/natutils/natutils.c
@@ -0,0 +1,4 @@
+#include <stdint.h>
+uint32_t map(uint32_t x, uint32_t in_min, uint32_t in_max, uint32_t out_min, uint32_t out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
